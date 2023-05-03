5452658aaf7b32682f35bcf09fc0fb046bb8de56
vs5c0 <venkatsai7722@gmail.com>
2023-05-03 23:39:06 +0530

    HTML Dom Manpulation

---
+++ Dom/app.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("code").style.color = "blue";
+document.getElementById("code").textContent = "web development";
+document.getElementById("form").style.background= "skyblue";
+function manipulate(){
+    document.getElementById("code").textContent="Artifical Intelligence";
+    document.getElementById("code").style.color="black";
+}
+function man(){
+    console.log(document.getElementById("code").style.color="violet");
+    console.log(document.getElementById("code").textContent = "Art Intelligent");
+}
