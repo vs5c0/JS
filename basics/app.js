2182ce2a97ee56b4398906aa1386d471934f4e55
vs5c0 <venkatsai7722@gmail.com>
2023-05-03 22:46:56 +0530

    basics

---
+++ basics/app.js
@@ -0,0 +1,46 @@
+console.log("Hello World@");
+alert("Warning");
+function name(){
+    console.log("\n\t JS");
+
+}
+name();
+
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(2,5));
+
+
+function mul(x,y){
+    result = x*y;
+    return result;
+};
+console.log(mul(5,7));
+
+function length(str){
+    return str.length;
+};
+console.log(length("vikram"));
+
+function even(num){
+    if(num%2===0){
+        return "even";
+    }
+    else{
+        return "odd";
+    }
+
+}; 
+console.log(even(7));
+
+function cp(str){
+    return str.toUpperCase()
+};
+console.log(cp("vikram"));
+
+
+
+
+
