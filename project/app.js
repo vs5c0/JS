741fa9c941931de318d7d3b1eeecf912bde69ff1
vs5c0 <venkatsai7722@gmail.com>
2023-05-04 11:55:20 +0530

    Project of the Day

---
+++ project/app.js
@@ -0,0 +1,15 @@
+
+function moon(){
+console.log(document.getElementById("moonImage").src ="./images/sun.png");
+
+};
+function change(){
+
+    console.log(document.getElementById("bg").style.background ="black");
+    console.log(document.getElementById("moonImage").src="./images/half-moon.png");
+
+};
+function Day(){
+    console.log(document.getElementById("bg").style.background="white");
+    console.log(document.getElementById("moonImage").src="./images/s.png");
+};
