f65fb1e0202063f455f56c92230939c2d672ab54
sai <127922969+vs5c0@users.noreply.github.com>
2023-07-01 15:40:06 +0530

    Add files via upload

---
+++ Basic HTML tags/Control.java
@@ -0,0 +1,13 @@
+class Control
+{
+public static void main(String[] args)
+{
+for (int i = 1;i<=7;i++)
+{
+System.out.println("Namah Pravathi Patha Eyyy ");
+System.out.println("Har Har Mahadev");
+System.out.println("Shambo Shankara...Mahadev");
+
+}
+}
+}
