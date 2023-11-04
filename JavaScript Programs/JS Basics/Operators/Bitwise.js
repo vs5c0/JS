fc04ff4e293e3daa74cda8309a313d32712fbb63
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-11-04 14:07:21 +0530

    Add files via upload

---
+++ JavaScript Programs/JS Basics/Operators/Bitwise.js
@@ -0,0 +1,19 @@
+//bitwise Operator--> used to perform the binary Operations(0's,1's)
+//There are mainly 2 types . They are
+// 1 Bitwise AND
+let form = 3;
+let vorm = 2;
+let cv = (form & vorm)
+console.log("Bitwise AND: ", cv)
+
+// 2 Bitwise OR
+let formm = 3;
+let vormm = 2;
+let cvm = (form | vorm)
+console.log("Bitwise OR: ", cvm)
+
+// Operator Precendence 
+//we can use the complex problems by using ()
+
+let jh = (10+(20*4)+20-5+(20**2));
+console.log("Operator Precedence: ",jh)
---
+++ JavaScript Programs/JS Basics/Operators/Comparison.js
@@ -0,0 +1,22 @@
+//Comparison Operator --> usesd to compare the relation between two numbers
+
+console.log("Greater than > : ", 2>3);
+
+console.log("Less than < : ", 4<2);
+
+console.log("Greater than or Equal to  >= : ", 4>=3);
+
+console.log("Less than  or Equal to <= : ", 5<=4);
+
+console.log("Equal to : ",5===5);
+
+console.log("Not Equal to  : ", 4!==5);
+
+// Equality Operator --> used to find the given value is equal or not
+// Two types .They are:
+// 1 Loose Equality
+// 2 strict Equality 
+let num = 1
+let str = '1'
+console.log("Loose Equality : ", num==str)
+console.log("Strict Equality: ", num===str)
