02f47d1c6cfedbfa64aefb74225ff16e6b807d1e
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-11-04 14:05:56 +0530

    Add files via upload

---
+++ JavaScript Programs/JS Basics/Operators/IncreDecreMentOperator.js
@@ -0,0 +1,29 @@
+// Increment --> used to increment the value by using (++)
+// Increment is mainly 2 types 
+// 1. Pre increment 
+
+let x = 10;
+console.log('Pre Increment Operator : ' , ++x);
+
+// 2. Post increment 
+
+let y = 10;
+console.log('Post Increment Operator : ' , y++);
+
+//Decrement --> used to decrease the values (--)
+
+// 1. Pre decrement
+let xx = 4;
+console.log('Pre Decrement Operator : ' , --xx);
+
+// 2. Post decrement 
+
+let yy = 10;
+console.log('Post Decrement Operator : ' , yy--);
+
+//Example Program 
+
+let ac = 5
+let bc = 10
+let ccxx = (++ac) * (--bc)
+console.log(ccxx)
