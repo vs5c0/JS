72fac54e27df16131a188ffa5c07e372392ac4e9
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-11-04 14:05:18 +0530

    Add files via upload

---
+++ JavaScript Programs/JS Basics/Operators/ArithematicOperators.js
@@ -0,0 +1,34 @@
+// Operators --> used to perform the operations
+
+// Arithematic Operators --> used to perform the operations like + - * / % **
+
+let a = 5;
+let b = 4 ;
+let c = a + b;
+console.log('Addition of Two Numbers : ', c);
+
+let A = 5;
+let w = 4 ;
+let z = a - b;
+console.log('Subraction of Two Numbers : ', z);
+
+let aa = 5;
+let bb = 4 ;
+let cc = a * b;
+console.log('Multiplication of Two Numbers : ', cc);
+
+let ax = 5;
+let bx = 4 ;
+let cx = a / b;
+console.log('Division of Two Numbers : ', cx);
+
+let ay = 20;
+let by = 5;
+let cy = a % b;
+console.log('Remainder of Two Numbers : ', cy);
+
+let az = 10;
+let bz = 2;
+let cz = a ** b;
+console.log('Power of Two Numbers : ', cz);
+
