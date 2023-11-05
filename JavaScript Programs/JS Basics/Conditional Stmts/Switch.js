630f00ec230f1f409e9a2ce300d0b87c4b23f1c2
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-11-05 14:08:39 +0530

    Add files via upload

---
+++ JavaScript Programs/JS Basics/Conditional Stmts/Switch.js
@@ -0,0 +1,13 @@
+let zzz = 'Feb';
+
+switch(zzz)
+{
+    case 'Jan':
+    console.log("It is Janauray");
+    break;
+    case 'Feb':
+    console.log("Feburay");
+    break;
+    default:
+        console.log("It is December");
+}
