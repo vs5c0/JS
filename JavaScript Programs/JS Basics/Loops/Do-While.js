7c0a1095c2cd5396b727b5cdc724d1b65e73f5e4
Venkat Sai <127922969+vs5c0@users.noreply.github.com>
2023-11-05 14:10:14 +0530

    Add files via upload

---
+++ JavaScript Programs/JS Basics/Loops/Do-While.js
@@ -0,0 +1,9 @@
+let p = 0;
+
+//do while loop --> it is an exit control loop 
+//at least one stmt can be executed
+// syntax cant have intialization, updation
+do{
+    console.log("This is a Tremendous Thing", p);
+    p++;
+} while(p<=10)
